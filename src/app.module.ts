5b868f35a79b72ec631cb9d48a8ccbeb6ce40b72
Lucas Marchiori <sinonsrt@hotmail.com>
2023-09-16 17:14:59 -0300

    feat: update env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { HotelQuotationModule } from './modules/hotel-quotation/hotel-quotation.module';
 import { ConfigModule } from '@nestjs/config';
 import { BrowserProvider, DateProvider } from './utils/providers';
+import env from './config/env';
 
 @Module({
-  imports: [ConfigModule.forRoot(), HotelQuotationModule],
+  imports: [ConfigModule.forRoot({ load: [env] }), HotelQuotationModule],
   controllers: [],
   providers: [BrowserProvider, DateProvider],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT');
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Scrapping API')
@@ -15,6 +19,6 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(port);
 }
 bootstrap();
